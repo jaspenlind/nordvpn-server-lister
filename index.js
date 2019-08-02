3634d75cad6708d6d04ee827528be63bd71c95e7
Johan <johan.aspenlind@b3.se>
2019-08-02 22:41:58 +0200

    Added script & linting

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const request = require("request-promise");
-const item = "item";
+const request = require('request-promise');
+const item = 'item';
 
 const handleHelp = args => {
-  if (args.filter(x => x === "-h").length > 0) {
+  if (args.filter(x => x === '-h').length > 0) {
     console.log(`
 usage: node ${args[1]} [parameters]
 
@@ -17,31 +17,32 @@ usage: node ${args[1]} [parameters]
 };
 
 const getServers = () => {
-  return request("https://api.nordvpn.com/server?limit=16384", { json: true });
+  return request('https://api.nordvpn.com/server?limit=16384', { json: true });
 };
 
+// eslint-disable-next-line no-new-func
 const dynamicCallback = (body, param = item) => new Function(param, `return ${body};`);
 
 const createFilter = args => {
-  let filterArgs = args.filter(x => x.startsWith("-filter."));
+  const filterArgs = args.filter(x => x.startsWith('-filter.'));
 
-  let predicate =
+  const predicate =
     filterArgs.length === 0
-      ? "true"
-      : filterArgs.map(x => x.replace(/\-filter/, item).replace(/\=/, "===")).join(" && ");
+      ? 'true'
+      : filterArgs.map(x => x.replace(/-filter/, item).replace(/=/, '===')).join(' && ');
 
   return dynamicCallback(predicate);
 };
 
 const createMap = args => {
-  let output = args.filter(x => x.startsWith("-output=")).map(x => x.substring(8));
+  const output = args.filter(x => x.startsWith('-output=')).map(x => x.substring(8));
 
-  let projection =
+  const projection =
     output.length === 0
       ? item
       : output.length === 1
-      ? `${item}.${output[0]}`
-      : "{ " + output.map(x => `${x}: ${item}.${x}`).join(",") + " };";
+        ? `${item}.${output[0]}`
+        : '{ ' + output.map(x => `${x}: ${item}.${x}`).join(',') + ' };';
 
   return dynamicCallback(projection);
 };
@@ -49,25 +50,21 @@ const createMap = args => {
 const printResult = (result, args) => {
   if (!result.length) return;
 
-  if (args.filter(x => x === "-raw").length > 0) {
-    result = result.length > 0 && typeof result[0] === "object" ? JSON.stringify(result) : result.join("\n");
+  if (args.filter(x => x === '-raw').length > 0) {
+    result = result.length > 0 && typeof result[0] === 'object' ? JSON.stringify(result) : result.join('\n');
   }
 
   console.log(result);
 };
 
 (async () => {
-  try {
-    let args = process.argv;
+  const args = process.argv;
 
-    handleHelp(args);
+  handleHelp(args);
 
-    let servers = await getServers();
+  const servers = await getServers();
 
-    let result = servers.filter(createFilter(args)).map(createMap(args));
+  const result = servers.filter(createFilter(args)).map(createMap(args));
 
-    printResult(result, args);
-  } catch (e) {
-    throw e;
-  }
+  printResult(result, args);
 })();
