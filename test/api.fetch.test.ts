7b87eb63c103fbac48dd16d380c1490ac66a9277
Johan <johan.aspenlind@b3.se>
2022-04-11 22:52:17 +0200

    chore: deps

--- test/api.fetch.test.ts
+++ test/api.fetch.test.ts
@@ -1,10 +1,10 @@
-import { fetch } from "../src";
+// import { fetch } from "../src";
 
 describe("api", () => {
   describe("fetch", () => {
-    it.todo("can fetch servers", async () => {
-      // const result = await fetch();
-      // expect(result.items.length).toBeGreaterThan(0);
-    });
+    it.todo("can fetch servers"); // , async () => {
+    // const result = await fetch();
+    // expect(result.items.length).toBeGreaterThan(0);
+    // });
   });
 });
